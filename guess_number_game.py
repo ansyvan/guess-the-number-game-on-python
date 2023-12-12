93403edd12b574314d6e8cd178672024f6c4ed4b
Andriana Syvanych <asyvanych@gmail.com>
2023-12-12 09:48:36 +0200

    Modified Guess the Number game

--- guess_number_game.py
+++ guess_number_game.py
@@ -6,112 +6,122 @@ import sys
 class GuessNumberGame:
     MIN_NUMBER = 1
     MAX_NUMBER = 20
-    MAX_GUESSES = 6
+    MAX_GUESSES = 5
 
     def __init__(self):
-        pass
+        self.player_name = ""
+        self.secret_number = None
+        self.answer_yes_no = None
+        self.guess = None
+        self.guesses_left = 0
+        self.guesses_taken = 0
+        self.guess_is_valid = False
     
-    # Beginning with asking the Player's name
-    def ask_player_name(self):
-        print("Hello there! May I know your name?")
-        self.player_name = input()
-        return self.player_name
 
+    # Beginning with asking the Player's name
     def run(self):
-        print(f"Welcome, {self.player_name}, to the 'Guess the Number' game! Would you like to play? (Yes/No)")
-        
+        print("Hello there! May I know your name?")
+        player_name = input()
+        print(f"Welcome, {player_name}, to the 'Guess the Number' game! Would you like to play? (Yes/No)")
+        if self.get_user_yes_no_input() == True:
+            self.play(player_name)
+        else:
+            self.exit_game()
+
 
     # Answering any Yes/No questions
-    def yes_no_choise(self):
+    def get_user_yes_no_input(self):
         self.answer_yes_no = input().lower()
         if self.answer_yes_no == "yes":
-            self.play_guess_number()
+            return True
         elif self.answer_yes_no == "no":
             print("I feel sorry to hear that. Hope to see you again soon.")
-            self.exit_game()
         else:
             print("Please type either 'Yes' or 'No'")
-            self.yes_no_choise()
-        return self.answer_yes_no
+
         
 
     def generate_secret_number(self):
-        print(f"Well, {self.player_name}, I am thinking of a number between {self.MIN_NUMBER} and {self.MAX_NUMBER}.")
         self.secret_number = random.randint(self.MIN_NUMBER, self.MAX_NUMBER)
+        return self.secret_number
 
 
     # Display appropriate message on remaining guesses.
-    def display_remaining_guesses(self):
-        for self.guesses_left in range(1, self.MAX_GUESSES):
-            if self.guesses_left == self.MAX_GUESSES - 1:
-                print(f"Take a guess! You have {self.guesses_left} guesses left.")
-            elif self.guesses_left != 1:
-                print(f"Try again! You have {self.guesses_left} guesses left.")
-            else:
-                print(f"Try again! You have {self.guesses_left} guess left.")
-        
-            self.validate_data()    # Validate the user's input.
+    def display_remaining_guesses(self, guesses_left):
+        if guesses_left == self.MAX_GUESSES:
+            print(f"Take a guess! You have {self.MAX_GUESSES} tries.")
+        elif guesses_left != 1:
+            print(f"Try again! You have {guesses_left} guesses left.")
+        else:
+            print(f"Try again! You have {guesses_left} guess left.")
 
-            # Check if the guessed number is correct.
-            if self.check_guess(self.guess):
-                break   # Exit the loop if the guess is correct.
-        
-        self.game_result(self.guess)
 
-    def calculate_guesses_left(self):
-            self.guesses_left = self.MAX_GUESSES - self.guesses_taken
-            return self.guesses_left
+    def calculate_guesses_left(self, guesses_taken):
+        self.guesses_left = (self.MAX_GUESSES + 1) - guesses_taken
+        return self.guesses_left
     
 
-
-
     def read_user_guess(self):
-        if self.guess == False:
-            self.guess = input()
-            self.validate_input_data()
-        else:
-            return self.guess
+        guess_is_valid = False
+        while not guess_is_valid:
+            guess = input()
+            guess_is_valid = self.validate_user_input(guess)
+        return int(guess)
         
-
-
-
     # Validate the input data is integer from MIN to MAX number
-    def validate_input_data(self):
-        while True:
-            self.guess = input()
-            try:
-                self.guess = int(self.guess)
-                if self.MIN_NUMBER <= self.guess <= self.MAX_NUMBER:
-                    return True    # Return the validated guess
-                else:
-                    print(f"Your number should be between {self.MIN_NUMBER} and {self.MAX_NUMBER}.")
-            except ValueError:
-                print("Please, enter an integer number.")
+    def validate_user_input(self, guess):
+        try:
+            guess = int(guess)
+            if self.MIN_NUMBER <= guess <= self.MAX_NUMBER:
+                return True    # Return the validated guess
+            else:
+                print(f"Your number should be between {self.MIN_NUMBER} and {self.MAX_NUMBER}.")
+        except ValueError:
+            print("Please, enter an integer number.")
+
 
     # Check if the number is correct
-    def check_guess(self):
-        if self.guess < self.secret_number:
+    def check_guess(self, guess, secret_number):
+        if guess < secret_number:
             print("Your number is too low.")
-        elif self.guess > self.secret_number:
+            return False
+        elif guess > secret_number:
             print("Your number is too high.")
+            return False
         else:
-            self.game_result(self.guess)  # This condition is the correct guess!
-            return True
+            return True  # This condition is the correct guess!
+            
             
     # Resuls of the game when Player guesses or loop is over
-    def game_result(self):
-        if self.guess == self.secret_number:
-            if self.guesses_taken == 1:
-                print(f"Wow, {self.player_name}! You made it in the first try!")
+    def game_result(self, player_name, guess, secret_number, guesses_taken):
+        if guess == secret_number:
+            if guesses_taken == 1:
+                print(f"Wow, {player_name}! You made it in the first try!")
             else:
-                print(f"Good job, {self.player_name}! You guessed my number in {self.guesses_taken} guesses!")
+                print(f"Good job, {player_name}! You guessed my number in {guesses_taken} guesses!")
         else:
-            print(f"No guesses left. The number I was thinking of was {self.secret_number}.")
+            print(f"No guesses left. The number I was thinking of was {secret_number}.")
+
         
+    def play(self, player_name):
+        print(f"Well, {player_name}, I am thinking of a number between {self.MIN_NUMBER} and {self.MAX_NUMBER}.")
+        secret_number = self.generate_secret_number()
+        guess = 0
+        for guesses_taken in range (1, self.MAX_GUESSES + 1):
+            guesses_left = self.calculate_guesses_left(guesses_taken)
+
+            self.display_remaining_guesses(guesses_left)
+            guess = self.read_user_guess()
+
+            # Check if the guessed number is correct
+            if self.check_guess(guess, secret_number):
+                break   # Exit the loop if the guess is correct
+
+        self.game_result(player_name, guess, secret_number, guesses_taken)
 
     def ask_to_play_again(self):
         print("Play again? (Yes/No)")
-        self.yes_no_choise()
+        self.get_user_yes_no_input()
 
     def exit_game(self):
         sys.exit()
